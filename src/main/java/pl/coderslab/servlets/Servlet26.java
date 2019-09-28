03e288c7c9edfcd5f400298b7569ebadc2f5a33a
Monika Misiewicz <zieba.monika@wp.pl>
2019-09-28 19:13:51 +0200

    Servlet26.java

---
+++ src/main/java/pl/coderslab/servlets/Servlet26.java
@@ -0,0 +1,37 @@
+package pl.coderslab.servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Map;
+
+@WebServlet("/servlet26")
+public class Servlet26 extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        Map<String, String> map = new HashMap<String, String>();
+
+        Enumeration headerNames = req.getHeaderNames();
+        while (headerNames.hasMoreElements()) {
+            String key = (String) headerNames.nextElement();
+            String value = req.getHeader(key);
+            map.put(key, value);
+        }
+
+        //
+        for (Map.Entry<String, String> entry : map.entrySet()) {
+            resp.getWriter().println(entry.getKey() + " : " + entry.getValue());
+        }
+
+
+    }
+
+
+}
