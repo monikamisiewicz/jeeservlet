7ff756787dc9f3f272e51a6057c7835b226eb2e4
Monika Misiewicz <zieba.monika@wp.pl>
2019-09-28 19:10:16 +0200

    Country.java

---
+++ src/main/java/pl/coderslab/servlets/Country.java
@@ -0,0 +1,20 @@
+package pl.coderslab.servlets;
+
+public class Country {
+
+    private String name;
+    private String capital;
+
+    public Country(String name, String capital) {
+        this.name = name;
+        this.capital = capital;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCapital() {
+        return capital;
+    }
+}
