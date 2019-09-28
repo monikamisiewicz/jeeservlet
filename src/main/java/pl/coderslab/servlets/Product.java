742ec0eee4b6bdd61176c6528f1101e33caf5ccc
Monika Misiewicz <zieba.monika@wp.pl>
2019-09-28 19:11:03 +0200

    Product.java

---
+++ src/main/java/pl/coderslab/servlets/Product.java
@@ -0,0 +1,26 @@
+package pl.coderslab.servlets;
+
+public class Product {
+
+    private String name;
+    private int quantity;
+    private double price;
+
+    public Product(String name, int quantity, double price) {
+        this.name = name;
+        this.quantity = quantity;
+        this.price = price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
